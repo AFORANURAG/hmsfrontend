d1e377eb2268ecbd63b05b0b48e95a4bff24a807
AFORANURAG <anuragupadhyay172912313@gmail.com>
2023-03-24 18:54:46 +0530

    urls chganged

--- hospitalmanagementsystemfrontend/src/backendurl/index.js
+++ hospitalmanagementsystemfrontend/src/backendurl/index.js
@@ -1,4 +1,4 @@
- const backendurl="https://hospitalmanegementsystem.onrender.com";
- const userdashboardurl="https://yogendracomponent.vercel.app";
+ const backendurl="https://hospitalmanagementsystembackend-gnz7.onrender.com";
+ const userdashboardurl="https://yogendracomponent-aforanurag.vercel.app";
  const doctorpanel = "https://kishor-react-app-aforanurag.vercel.app";
  export {backendurl,userdashboardurl,doctorpanel};
